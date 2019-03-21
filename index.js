f5c2c7e4cc99d56808c8762a631df37dc445522b
vinnyfs89 <viniciusfesil@gmail.com>
2019-03-21 12:28:33 -0300

    [UPDATE] Showing active users while connect ou disconnect on socket  channel.

--- index.js
+++ index.js
@@ -12,17 +12,26 @@ io.on('connection', function (socket) {
     // console.log(socket.client.id);
     console.log('an user connected');
     clients.push({clientId: socket.client.id})
+    console.log(`------`)
+    console.log(`users active ${clients.length}`)
+    console.log(`------`)
 
     socket.on('disconnect', function () {
         const index = clients.findIndex(client => client.clientId === socket.client.id);
         clients.splice(index, 1);
         console.log('user disconnected');
-
-    });
-
-    socket.on('active_users', () => {
+        console.log(`------`)
         console.log(`users active ${clients.length}`)
+        console.log(`------`)
     });
+
+    // socket.on('active_users', () => {
+    //     console.log(`users active ${clients.length}`)
+    // });
+
+    // socket.on('chat message', function(msg){
+    //     console.log('message: ' + msg);
+    // });
 });
 
 http.listen(3000, function () {
