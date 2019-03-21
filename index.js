059f97f0a3b10e92d11cb6d5ae36d51ae33eb6c8
vinnyfs89 <viniciusfesil@gmail.com>
2019-03-21 13:04:26 -0300

    [UPDATE] Broadcasting events to general chat when clients are connected or disconnected.

--- index.js
+++ index.js
@@ -11,15 +11,18 @@ app.get('/', function (req, res) {
 io.on('connection', function (socket) {
     // console.log(socket.client.id);
     console.log('an user connected');
+
     clients.push({clientId: socket.client.id})
 
     io.emit('connectedUsers', clients.length);
+    io.emit('clientConnected', { clientId: socket.client.id });
 
     socket.on('disconnect', function () {
         const index = clients.findIndex(client => client.clientId === socket.client.id);
         clients.splice(index, 1);
         console.log('user disconnected');
         io.emit('connectedUsers', clients.length);
+        io.emit('clientDisconnected', { clientId: socket.client.id });
     });
 
     // socket.on('active_users', () => {
